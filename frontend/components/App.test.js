e00928d42ba57897bb877a533078794a15125002
delimmaemma <calvinhtml@gmail.com>
2023-07-29 01:21:15 -0500

    Removed unnecessary imports from app.test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
 import AppFunctional from './AppFunctional';
 
 test('App Class Renders', () => {
