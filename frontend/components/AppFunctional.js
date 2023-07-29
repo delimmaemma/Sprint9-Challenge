2b99aed886f92921d34e1b1e1a5be424209543a0
delimmaemma <calvinhtml@gmail.com>
2023-07-29 01:00:02 -0500

    All errors seem to be fixed.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,183 +1,141 @@
-// Can go OOB
-// Error messages don't accurately show
-// Active square can wrap
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import React, { useState } from 'react'
-import axios from 'axios'
+const initialMessage = '(2, 2)';
+const initialResponse = '';
+const initialEmail = '';
+const initialSteps = 0;
+const initialIndex = 4;
+const URL = 'http://localhost:9000/api/result';
 
-const initialMessage = '(2, 2)'
-const initialResponse = ''
-const initialSteps = 0
-const initialIndex = 4
-const initialState = {
-  x: 2,
-  y: 2,
-  steps: 0,
-  email: ''
-}
-const URL = 'http://localhost:9000/api/result'
-let xcoord = 2
-let ycoord = 2
-let xfake = 2
-let yfake = 2
+const AppFunctional = ({ className }) => {
+  const [x, setX] = useState(2);
+  const [y, setY] = useState(2);
+  const [steps, setSteps] = useState(initialSteps);
+  const [email, setEmail] = useState(initialEmail);
+  const [message, setMessage] = useState(initialMessage);
+  const [response, setResponse] = useState(initialResponse);
+  const [index, setIndex] = useState(initialIndex);
 
-export default function AppFunctional(props) {
-
-  const [message, setMessage] = useState(initialMessage)
-  const [index, setIndex] = useState(initialIndex)
-  const [movement, setMovement] = useState(initialSteps)
-  const [state, setState] = useState(initialState)
-  const [response, setResponse] = useState(initialResponse)
-
-  function getXY(press) {
-    setResponse(initialResponse)
-    //Left
-    if(press === 1 && ycoord - 1 > 0) {
-      ycoord -= 1;
-      setResponse(initialResponse)
-    }
-    //Up
-    else if(press === 2 && xcoord - 1 > 0) {
-      xcoord -= 1;
-      setResponse(initialResponse)
-    }
-    //Right
-    else if(press === 3 && ycoord + 1 < 4) {
-      ycoord += 1;
-      setResponse(initialResponse)
-    }
-    //Down
-    else if(press === 4 && xcoord + 1 < 4) {
-      xcoord += 1;
-      setResponse(initialResponse)
-    }
-
-    if(press === 1) yfake -= 1;
-    else if(press === 2) xfake -= 1
-    else if(press === 3) yfake += 1
-    else if(press === 4) xfake += 1
-
-
-    if(press === 1 && yfake <= 0) {
-      setResponse("You can't go up")
-      yfake = ycoord
-    }
-    else if(press === 2 && xfake <= 0) {
-      setResponse("You can't go left")
-      xfake = xcoord
-    }
-    else if(press === 3 && yfake >= 4) {
-      setResponse("You can't go down")
-      yfake = ycoord
-    }
-    else if(press === 4 && xfake >= 4) {
-      setResponse("You can't go right")
-      xfake = xcoord
-    }
-    
-    getNextIndex(press)
-  }
-
-  function getXYMessage(press) {
-    getXY(press)
-  }
-
-  function reset() {
-    xcoord = 2;
-    ycoord = 2;
+  const reset = () => {
+    setX(2);
+    setY(2);
     setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
     setIndex(initialIndex);
-    setState(initialState)
-    setMovement(initialSteps)
-    setResponse(initialResponse)
-  }
+    setResponse(initialResponse);
+  };
 
-  function getNextIndex(direction) {
-
+  const getXY = (press) => {
+    setResponse(initialResponse);
     const changes = [
       [0, -1], // Left
       [-1, 0], // Up
-      [0, 1], // Right
-      [1, 0], // Down
+      [0, 1],  // Right
+      [1, 0],  // Down
     ];
 
+    const [dx, dy] = changes[press - 1];
+    const yfake = y + dy;
+    const xfake = x + dx;
+
+    let response = initialResponse;
+    if (yfake <= 0) {
+      response = "You can't go up";
+    } else if (xfake <= 0) {
+      response = "You can't go left";
+    } else if (yfake >= 4) {
+      response = "You can't go down";
+    } else if (xfake >= 4) {
+      response = "You can't go right";
+    }
+
+    if (response === initialResponse) {
+      setX((prevX) => prevX + dx);
+      setY((prevY) => prevY + dy);
+      setResponse(initialResponse);
+      getNextIndex(press);
+    } else {
+      setResponse(response);
+    }
+  };
+
+  const getNextIndex = (direction) => {
+    const changes = [
+      [0, -1], // Left
+      [-1, 0], // Up
+      [0, 1],  // Right
+      [1, 0],  // Down
+    ];
+  
     const [dx, dy] = changes[direction - 1];
-    const nextIndex = index + dx + (dy * 3);
+    const nextX = x + dx;
+    const nextY = y + dy;
+  
+    if (nextX >= 1 && nextX <= 3 && nextY >= 1 && nextY <= 3) {
+      setX(nextX);
+      setY(nextY);
+      setIndex(index + (dx + dy * 3));
+      move();
+    }
+  };
 
-    const coordinatesMap = {
-      2: { x: 3, y: 1 },
-      3: { x: 1, y: 2 },
-      5: { x: 3, y: 2 },
-      6: { x: 1, y: 3 },
-    };
+  const move = () => {
+    setSteps((prevSteps) => prevSteps + 1);
+  };
 
-    const { x, y } = coordinatesMap[nextIndex] || {};
-    setState((prevState) => ({
-      ...prevState,
-      x: x || prevState.x,
-      y: y || prevState.y,
-    }));
-    setIndex(nextIndex);
+  const onChange = (evt) => {
+    setEmail(evt.target.value);
+  };
 
-    move(direction)
-  }
-
-  function move() {
-    setMessage(`(${xcoord}, ${ycoord})`)
-    setState({...state, x: xcoord, y: ycoord, steps: movement})
-  }
-
-  function onChange(evt) {
-    setState({...state, email: evt.target.value})
-  }
-
-  function onSubmit(evt) {
-    evt.preventDefault()
-    axios.post(URL, state)
-      .then(res => {
-        setResponse(res.data.message)
-        xcoord = 2;
-        ycoord = 2;
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    axios.post(URL, { x, y, steps, email })
+      .then((res) => {
+        setResponse(res.data.message);
+        setX(2);
+        setY(2);
         setMessage(initialMessage);
+        setEmail(initialEmail);
+        setSteps(initialSteps);
         setIndex(initialIndex);
-        setState(initialState)
-        setMovement(initialSteps)
       })
-      .catch(err => {
-        setResponse(err.response.data.message)
-        setState({...state, email: ''})
-      })
-  }
+      .catch((err) => {
+        setEmail('');
+        setResponse(err.response.data.message);
+      });
+  };
 
   return (
-    <div id="wrapper" className={props.className}>
+    <div id="wrapper" className={className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates {message}</h3>
-        <h3 id="steps">You moved {movement} {movement !== 1 ? `times` : `time`}</h3>
+        <h3 id="coordinates">Coordinates ({y}, {x})</h3>
+        <h3 id="steps">You moved {steps} {steps !== 1 ? `times` : `time`}</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
-              {idx === index ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+            {idx === index ? 'B' : null}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message">{response}</h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={() => {getXYMessage(2)}}>LEFT</button>
-        <button id="up" onClick={() => {getXYMessage(1)}}>UP</button>
-        <button id="right" onClick={() => {getXYMessage(4)}}>RIGHT</button>
-        <button id="down" onClick={() => {getXYMessage(3)}}>DOWN</button>
-        <button id="reset" onClick={() => {reset()}}>reset</button>
+        <button id="left" onClick={() => { getXY(2) }}>LEFT</button>
+        <button id="up" onClick={() => { getXY(1) }}>UP</button>
+        <button id="right" onClick={() => { getXY(4) }}>RIGHT</button>
+        <button id="down" onClick={() => { getXY(3) }}>DOWN</button>
+        <button id="reset" onClick={() => { reset() }}>reset</button>
       </div>
       <form onSubmit={onSubmit}>
-        <input id="email" type="email" placeholder="type email" onChange={onChange} value = {state.email}></input>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
-}
+  );
+};
+
+export default AppFunctional;
